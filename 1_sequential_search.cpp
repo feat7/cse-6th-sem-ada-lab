f984d1c877b27dbfd3c325d8bafd1512dae3476c
feat7 <vinaykhobragade99@gmail.com>
2019-01-25 10:57:48 +0530

    add: linkedlist sequential search

--- 1_sequential_search.cpp
+++ 1_sequential_search.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
 int linear_search(int arr[], int item, int len)
@@ -14,11 +15,30 @@ int linear_search(int arr[], int item, int len)
     return 0;
 }
 
+int linked_list_search(list<int> linked_list, int item)
+{
+    list<int>::iterator it;
+    int count = 1;
+    for (it = linked_list.begin(); it != linked_list.end(); ++it)
+    {
+        if (item == *it)
+        {
+            cout << "Item found at " << count;
+            return 1;
+        }
+        count++;
+    }
+    cout << "Item not found at " << *it;
+    return 0;
+}
+
 int main()
 {
     int arr[] = {10, 40, 333, 30, 21, 33, 57, 88};
+    list<int> linked_list = {10, 40, 333, 30, 21, 33, 57, 88};
 
     int len = sizeof(arr) / sizeof(arr[0]);
 
-    linear_search(arr, 30, len);
+    // linear_search(arr, 30, len);
+    linked_list_search(linked_list, 30);
 }
